5794868967811661c51e7c5a1ee2db47256b0122
idonans <idonans@126.com>
2016-04-12 16:00:06 +0800

    add AppContext

---
+++ acommon/src/main/java/com/idonans/acommon/AppContext.java
@@ -0,0 +1,21 @@
+package com.idonans.acommon;
+
+import android.content.Context;
+
+/**
+ * 在程序启动式设置 Context, 通常在 Application#onCreate, ContentProvider#onCreate
+ * Created by idonans on 16-4-12.
+ */
+public class AppContext {
+
+    private static Context sContext;
+
+    public static Context getContext() {
+        return sContext;
+    }
+
+    public static void setContext(Context context) {
+        sContext = context.getApplicationContext();
+    }
+
+}
