23d6c16c4555fa234fda81574184c39c1c5c5901
idonans <idonans@126.com>
2016-04-13 18:27:22 +0800

    add StorageManager

---
+++ acommon/src/main/java/com/idonans/acommon/data/StorageManager.java
@@ -0,0 +1,72 @@
+package com.idonans.acommon.data;
+
+import android.support.annotation.CheckResult;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+
+import com.idonans.acommon.internal.db.SimpleDB;
+import com.idonans.acommon.lang.CommonLog;
+
+/**
+ * 数据存储服务
+ * <p/>
+ * 不同进程间的数据不同，不能使用此存储实现进程间共享数据。
+ * 此处提供了 setting 和 cache 两个不同的存储空间，各自独立运行。
+ * <p/>
+ * Created by idonans on 16-4-13.
+ */
+public class StorageManager {
+
+    private static class InstanceHolder {
+
+        private static final StorageManager sInstance = new StorageManager();
+
+    }
+
+    public static StorageManager getInstance() {
+        return InstanceHolder.sInstance;
+    }
+
+    private static final String TAG = "StorageManager";
+    private final SimpleDB mDBSetting;
+    private final SimpleDB mDBCache;
+
+    private StorageManager() {
+        mDBSetting = new SimpleDB("setting");
+        mDBCache = new SimpleDB("cache");
+
+        // 数据库启动时, 做一次 trim 操作.
+        int settingTrimSize = mDBSetting.trim(5000);
+        CommonLog.d(TAG + " setting trim size:" + settingTrimSize);
+        int cacheTrimSize = mDBCache.trim(5000);
+        CommonLog.d(TAG + " cache trim size:" + cacheTrimSize);
+    }
+
+    public void setSetting(@Nullable String key, @Nullable String value) {
+        mDBSetting.set(key, value);
+    }
+
+    @CheckResult
+    public String getSetting(@Nullable String key) {
+        String value = mDBSetting.get(key);
+        if (!TextUtils.isEmpty(value)) {
+            mDBSetting.touch(key);
+        }
+        return value;
+    }
+
+    public void setCache(@Nullable String key, @Nullable String value) {
+        mDBCache.set(key, value);
+    }
+
+    @CheckResult
+    public String getCache(@Nullable String key) {
+        String value = mDBCache.get(key);
+        if (!TextUtils.isEmpty(value)) {
+            mDBCache.touch(key);
+        }
+        return value;
+    }
+
+}
+
