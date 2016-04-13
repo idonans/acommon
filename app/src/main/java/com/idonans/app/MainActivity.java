e8f8006038d5c2842e407290ba14ac18b64b21d9
idonans <idonans@126.com>
2016-04-13 18:35:35 +0800

    add demo for StorageManager at MainActivity

--- app/src/main/java/com/idonans/app/MainActivity.java
+++ app/src/main/java/com/idonans/app/MainActivity.java
@@ -2,11 +2,16 @@ package com.idonans.app;
 
 import android.content.ContextWrapper;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.TextView;
 
 import com.idonans.acommon.app.CommonActivity;
 import com.idonans.acommon.data.ProcessManager;
+import com.idonans.acommon.data.StorageManager;
 import com.idonans.acommon.util.DimenUtil;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends CommonActivity {
 
@@ -26,7 +31,30 @@ public class MainActivity extends CommonActivity {
         builder.append("sp2px 0.2sp->").append(DimenUtil.sp2px(0.2f)).append("\n");
         builder.append("text context info:").append(text.getContext().getClass()).append("\n");
         builder.append("text context base info:").append(((ContextWrapper) text.getContext()).getBaseContext().getClass()).append("\n");
+        builder.append("app first run time:").append(getAppFirstRunTime()).append("\n");
+        builder.append("app last run time:").append(getAppLastRunTime()).append("\n");
         text.setText(builder);
+    }
+
+    private String getAppFirstRunTime() {
+        String key = "first run time";
+        String time = StorageManager.getInstance().getSetting(key);
+        if (TextUtils.isEmpty(time)) {
+            time = getTimeNow();
+            StorageManager.getInstance().setSetting(key, time);
+        }
+        return time;
+    }
+
+    private String getAppLastRunTime() {
+        String key = "last run time";
+        String time = StorageManager.getInstance().getSetting(key);
+        StorageManager.getInstance().setSetting(key, getTimeNow());
+        return time;
+    }
+
+    private String getTimeNow() {
+        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
     }
 
 }
