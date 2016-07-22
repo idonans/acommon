f561df06bdc750985e549dbdb1ee2ba673c77576
pengji <qwe1234A>
2016-07-22 18:33:09 +0800

    hide or show status bar need api >= 19

--- acommon/src/main/java/com/idonans/acommon/util/SystemUtil.java
+++ acommon/src/main/java/com/idonans/acommon/util/SystemUtil.java
@@ -161,13 +161,17 @@ public class SystemUtil {
     }
 
     public static void hideStatusBar(Window window) {
-        window.getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        if (Build.VERSION.SDK_INT >= 19) {
+            window.getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        }
     }
 
     public static void showStatusBar(Window window) {
-        window.getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        if (Build.VERSION.SDK_INT >= 19) {
+            window.getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
     }
 
     public static void setStatusBarTransparent(Window window) {
