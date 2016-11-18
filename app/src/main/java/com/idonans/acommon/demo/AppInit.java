14998092232cf04110019e7958fd307b29f629f9
pengji <qwe1234A>
2016-11-18 10:57:50 +0800

    add AppInit

---
+++ app/src/main/java/com/idonans/acommon/demo/AppInit.java
@@ -0,0 +1,72 @@
+package com.idonans.acommon.demo;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.idonans.acommon.App;
+
+/**
+ * Created by idonans on 2016/11/18.
+ */
+
+public final class AppInit {
+
+    private static final Object LOCK = new Object();
+    private static boolean sInit;
+
+    public static void init(Context context) {
+        if (sInit) {
+            return;
+        }
+
+        synchronized (LOCK) {
+            if (!sInit) {
+                new App.Config.Builder()
+                        .setContext(context)
+                        .setBuildConfigAdapter(new BuildConfigAdapterImpl())
+                        .build().init();
+                sInit = true;
+            }
+        }
+    }
+
+    private static class BuildConfigAdapterImpl implements App.BuildConfigAdapter {
+
+        @Override
+        public int getVersionCode() {
+            return BuildConfig.VERSION_CODE;
+        }
+
+        @Override
+        public String getVersionName() {
+            return BuildConfig.VERSION_NAME;
+        }
+
+        @Override
+        public String getLogTag() {
+            return BuildConfig.APPLICATION_ID;
+        }
+
+        @Override
+        public String getPublicSubDirName() {
+            return BuildConfig.APPLICATION_ID;
+        }
+
+        @Override
+        public String getChannel() {
+            return "default_channel";
+        }
+
+        @Override
+        public int getLogLevel() {
+            return Log.DEBUG;
+        }
+
+        @Override
+        public boolean isDebug() {
+            return BuildConfig.DEBUG;
+        }
+
+    }
+
+}
--- app/src/main/java/com/idonans/acommon/demo/MainApplication.java
+++ app/src/main/java/com/idonans/acommon/demo/MainApplication.java
@@ -2,8 +2,6 @@ package com.idonans.acommon.demo;
 
 import android.app.Application;
 
-import com.idonans.acommon.App;
-
 /**
  * App 入口
  * Created by idonans on 16-4-12.
@@ -13,10 +11,7 @@ public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        new App.Config.Builder()
-                .setContext(this)
-                .setBuildConfigAdapter(new BuildConfigAdapterImpl())
-                .build().init();
+        AppInit.init(this);
     }
 
 }
