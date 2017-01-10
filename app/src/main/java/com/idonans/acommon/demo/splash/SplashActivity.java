26111da8bcc6b8eec093ed290d5c97d7129ccc24
pengji <qwe1234A>
2017-01-10 14:28:37 +0800

    improve SplashActivity app for acommon demo

--- app/src/main/java/com/idonans/acommon/demo/splash/SplashActivity.java
+++ app/src/main/java/com/idonans/acommon/demo/splash/SplashActivity.java
@@ -1,15 +1,20 @@
 package com.idonans.acommon.demo.splash;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.idonans.acommon.demo.R;
 import com.idonans.acommon.demo.app.BaseActivity;
+import com.idonans.acommon.util.SystemUtil;
+import com.idonans.acommon.util.ViewUtil;
 
 /**
  * Created by idonans on 2016/11/21.
  */
 
 public class SplashActivity extends BaseActivity {
+
+    private TextView mMessageShow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,31 @@ public class SplashActivity extends BaseActivity {
         }
 
         setContentView(R.layout.splash_activity);
+
+        mMessageShow = ViewUtil.findViewByID(this, R.id.message_show);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (isAvailable()) {
+            return;
+        }
+
+        mMessageShow.postOnAnimation(new Runnable() {
+            @Override
+            public void run() {
+                syncSoftKeyboardStatus();
+            }
+        });
+    }
+
+    private void syncSoftKeyboardStatus() {
+        if (SystemUtil.isSoftKeyboardShown(this)) {
+            mMessageShow.setText("soft keyboard is shown");
+        } else {
+            mMessageShow.setText("soft keyboard is hidden");
+        }
     }
 
 }
